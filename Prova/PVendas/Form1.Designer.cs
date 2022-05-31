3db2a9d1dcd630ecd909ce4200b724be44f01d5a
Antonio Carlos Fernandes Gomes Filho <47403160+antonioN313@users.noreply.github.com>
2022-05-31 10:37:09 -0300

    Add files via upload

---
+++ Prova/PVendas/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PVendas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.lisbxrelatorio = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(45, 147);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(184, 171);
+            this.btnVerificar.TabIndex = 0;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // lisbxrelatorio
+            // 
+            this.lisbxrelatorio.FormattingEnabled = true;
+            this.lisbxrelatorio.Location = new System.Drawing.Point(289, 84);
+            this.lisbxrelatorio.Name = "lisbxrelatorio";
+            this.lisbxrelatorio.Size = new System.Drawing.Size(344, 329);
+            this.lisbxrelatorio.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 481);
+            this.Controls.Add(this.lisbxrelatorio);
+            this.Controls.Add(this.btnVerificar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.ListBox lisbxrelatorio;
+    }
+}
+
---
+++ Prova/PVendas/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+namespace PVendas
+{
+
+    //Alunos Antonio Carlos Fernandes Gomes Filho e Elais Marques Rocha
+    //RA dos Alunos: 0030482121008 e 0030482121049
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            int N = 8; 
+            int i,j;
+            double[,] PVendas = new double[N, 4];
+            string aux;
+            double semanaVendas;
+            double mesVendas = 0;
+            double TotalmesVendas = 0;
+            
+
+            for(i=0;i<N;i++)
+            {
+                for(j=0;j<4;j++)
+                {
+                    aux = Interaction.InputBox("Digite o total da semana " + (j + 1) + " do mês " + (i + 1), "Entrada dos Valores");
+
+                    if(!double.TryParse(aux, out PVendas[i,j]))
+                    {
+                        MessageBox.Show("Valor Invalido");
+
+                        j--;
+                    }
+                    semanaVendas = PVendas[i, j];
+                    lisbxrelatorio.Items.Add("Total do Mês " + (i + 1) + " Semana "+(j+1)+" : R$" + semanaVendas.ToString("N2"));
+
+                    mesVendas += PVendas[i, j];
+
+                }
+                lisbxrelatorio.Items.Add(">>Total Mês R$" + mesVendas.ToString("N2"));
+                mesVendas = 0;
+                lisbxrelatorio.Items.Add("---------------------------------");
+            }
+            for(i=0;i<N;i++)
+            {
+                for(j=0;j<4;j++)
+                {
+                    TotalmesVendas += PVendas[i, j];
+                }
+            }
+            lisbxrelatorio.Items.Add(">>Total Geral R$" + TotalmesVendas.ToString("N2"));
+            
+        }
+    }
+}
